4a7d37d2b77e5d84a00d9f539f475f6b184c69ab
alexey.cigelnikov <alexeyt3485@gmail.com>
2018-06-25 22:44:38 +0700

    main interfaces

---
+++ Planets/Planets/ICamera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets
+{
+    public interface ICamera
+    {
+        int Top { get; }
+        int Left { get; }
+        int Bottom { get; }
+        int Right { get; }
+        void SetPosX(int x);
+        void SetPosY(int y);
+        void SetZoom(int zoom);
+        int GetZoom();
+    }
+}
---
+++ Planets/Planets/ISector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets
+{
+    public interface ISector
+    {
+        int GetPlanet(int index);
+        int GetX { get; }
+        int GetY { get; }
+    }
+}
