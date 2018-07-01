41e49c20baaf81af2c510cb83c1b6001626d12c9
alexey.cigelnikov <alexeyt3485@gmail.com>
2018-07-01 17:55:01 +0700

    gui player info added

---
+++ UnityPlanets/Assets/Scripts/GUI/PlayerDataController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using Planets;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class PlayerDataController : MonoBehaviour, IPlayerListener
+{
+    [SerializeField] private Text mTextX;
+    [SerializeField] private Text mTextY;
+    [SerializeField] private Text mScore;
+    [SerializeField] private Text mZoomText;
+    [SerializeField] private Text mZoomText2;
+
+    [Inject] private readonly IPlayer mPlayer;
+	// Use this for initialization
+	void Start () {
+		mPlayer.AddListener(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PositionCanged(int posX, int posY)
+    {
+        mTextX.text = posX.ToString();
+        mTextY.text = posY.ToString();
+    }
+}
