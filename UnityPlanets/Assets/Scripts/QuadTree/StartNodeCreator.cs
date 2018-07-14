12bd3e408a0a53c391b3384617ad136b1b4a481d
alexey.cigelnikov <alexeyt3485@gmail.com>
2018-07-14 19:16:12 +0700

    refactoring

--- UnityPlanets/Assets/Scripts/QuadTree/StartNodeCreator.cs
+++ UnityPlanets/Assets/Scripts/QuadTree/StartNodeCreator.cs
@@ -37,76 +37,12 @@ namespace QuadTree
             var planetDataProvider = mPlanetFactoryCreator.CreatePlanetFactory();
             if (levels > 0)
             {
-                var processorCount = Environment.ProcessorCount;
-                waitHandles = new WaitHandle[processorCount];
-                var subsectors = 1;
-                var currentLevelMain = levels;
-                var subsectorSideSize = segmentSize;
-                var subSectorsInRaw = 1;
-                var subsectorLevels = levels;
-                while (subsectors < processorCount)
-                {
-                    subsectors *= 4;
-                    subSectorsInRaw *= 2;
-                    subsectorSideSize /= 2;
-                    subsectorLevels -= 1;
-                }
-                var subsectorsPerProc = subsectors / processorCount;
-                if (subsectors % processorCount != 0)
-                {
-                    subsectorsPerProc += 1;
-                }
-                var allSubsectors = new IQuadTreeNode[subsectors];
-                for (int i = 0; i < processorCount; ++i)
-                {
-                    var startIndex = i * subsectorsPerProc;
-                    var endIndex = startIndex + subsectorsPerProc;
-                    waitHandles[i] = new AutoResetEvent(false);
-                    var waitCallback = new WaitCallback((Object state) =>
-                    {
-                        AutoResetEvent are = (AutoResetEvent)state;
-                        try
-                        {
-                            for (int j = startIndex; j < endIndex; ++j)
-                            {
-                                var posY = segmentSize / 2f - (j / subSectorsInRaw) * subsectorSideSize -
-                                           subsectorSideSize / 2f;
-                                var posX = -segmentSize / 2f + (j - subSectorsInRaw*(j / subSectorsInRaw)) * subsectorSideSize +
-                                           subsectorSideSize / 2f;
-
-                                var allLeafs = CreateStartLeafs(subsectorSideSize, posX, posY);
-                                var currentLevel = subsectorLevels;
-                                var startSize = (int) Math.Pow(2, subsectorLevels);
-                                var mergedNodes = allLeafs;
-                                while (mergedNodes.Count != 1)
-                                {
-                                    mergedNodes = Merge(mergedNodes, startSize);
-                                    startSize /= 2;
-                                    --currentLevel;
-                                }
-                                allSubsectors[j] = mergedNodes[0];
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            
-                        }
-
-                        
-                        are.Set();
-                    });
-                    ThreadPool.QueueUserWorkItem(waitCallback, waitHandles[i]);
-                }
-                WaitHandle.WaitAll(waitHandles);
-                var allSubsectorsList = new List<IQuadTreeNode>(subsectors);
-                allSubsectorsList.AddRange(allSubsectors);
-                var startSizeMaint = (int)Math.Pow(2, levels-subsectorLevels);
+                var allSubsectorsList = CreateQuadTreeNodesOnMultithreds(segmentSize);
                 while (allSubsectorsList.Count != 1)
                 {
-                    allSubsectorsList = Merge(allSubsectorsList, startSizeMaint);
-                    subsectorSideSize /= 2;
-                    --currentLevelMain;
+                    allSubsectorsList = Merge(allSubsectorsList);
                 }
+                
                 rootNode = allSubsectorsList[0];
             }
             else
@@ -118,8 +54,74 @@ namespace QuadTree
         }
 
         [NotNull]
-        private List<IQuadTreeNode> Merge([NotNull] List<IQuadTreeNode> mergedNodes, int startRawSize)
+        private List<IQuadTreeNode> CreateQuadTreeNodesOnMultithreds(int segmentSize)
         {
+            var processorCount = Environment.ProcessorCount;
+            waitHandles = new WaitHandle[processorCount];
+            var subsectors = 1;
+            var subsectorSideSize = segmentSize;
+            var subSectorsInRaw = 1;
+            while (subsectors < processorCount)
+            {
+                subsectors *= 4;
+                subSectorsInRaw *= 2;
+                subsectorSideSize /= 2;
+            }
+            var subsectorsPerProc = subsectors / processorCount;
+            if (subsectors % processorCount != 0)
+            {
+                subsectorsPerProc += 1;
+            }
+
+            var allSubsectors = new IQuadTreeNode[subsectors];
+            for (int i = 0; i < processorCount; ++i)
+            {
+                var startIndex = i * subsectorsPerProc;
+                var endIndex = startIndex + subsectorsPerProc;
+                waitHandles[i] = new AutoResetEvent(false);
+                var waitCallback = new WaitCallback((Object state) =>
+                {
+                    AutoResetEvent are = (AutoResetEvent)state;
+                    try
+                    {
+                        for (var j = startIndex; j < endIndex; ++j)
+                        {
+                            var posY = segmentSize / 2f - (j / subSectorsInRaw) * subsectorSideSize -
+                                       subsectorSideSize / 2f;
+                            var posX = -segmentSize / 2f + (j - subSectorsInRaw * (j / subSectorsInRaw)) * subsectorSideSize +
+                                       subsectorSideSize / 2f;
+
+                            var allLeafs = CreateStartLeafs(subsectorSideSize, posX, posY);
+                            var mergedNodes = allLeafs;
+                            while (mergedNodes.Count != 1)
+                            {
+                                mergedNodes = Merge(mergedNodes);
+                            }
+                            allSubsectors[j] = mergedNodes[0];
+                        }
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                    are.Set();
+                });
+                ThreadPool.QueueUserWorkItem(waitCallback, waitHandles[i]);
+            }
+            WaitHandle.WaitAll(waitHandles);
+            var allSubsectorsList = new List<IQuadTreeNode>(subsectors);
+            allSubsectorsList.AddRange(allSubsectors);
+            return allSubsectorsList;
+        }
+
+        [NotNull]
+        private List<IQuadTreeNode> Merge([NotNull] List<IQuadTreeNode> mergedNodes)
+        {
+            int startRawSize = 1;
+            do
+            {
+                startRawSize *= 2;
+            } while (startRawSize * startRawSize < mergedNodes.Count);
 
             var endRawSize = startRawSize / 2;
             var resultSize = endRawSize * endRawSize;
