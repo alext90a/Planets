8afadbd22923d23685e798d1ad7908207f5b8dce
alexey.cigelnikov <alexeyt3485@gmail.com>
2018-07-15 23:36:25 +0700

    ui fix

---
+++ UnityPlanets/Assets/ExitController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitController : MonoBehaviour
+{
+    [NotNull][SerializeField]
+    private Button mExitButton;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    mExitButton.onClick.AddListener(Exit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Exit()
+    {
+        Application.Quit();
+    }
+}
